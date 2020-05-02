5f13d290788659c1c1050edd62a59dbcf41b8304
Prabin Kayastha <princekayastha@gmail.com>
2020-05-02 22:09:08 +0545

    Refactored the code to export the topo adjusted data in corresponding files.

--- UtilityFunctions.py
+++ UtilityFunctions.py
@@ -73,7 +73,7 @@ def write_asc_data_file(asc_data, output_file_path):
         writer.writerows(asc_data)
 
 
-def create_topo_adjusted_data(data_file_path):
+def export_topo_adjusted_data(data_file_path):
     """
     Returns a dictionary of .asc filename and its corresponding AscData type object.
     Adjusts the data values in object of type AscData class with the topo.asc data and
@@ -82,20 +82,12 @@ def create_topo_adjusted_data(data_file_path):
     return: Dictionary of .asc filename and its corresponding AscData type object.
     """
     data_file_objects_dict = {}
-    if data_file_path:
-        print(str(len(data_file_path)) + " data files found!!!")
 
-        for data_file_location in data_file_path:
-            data_file_objects_dict[extract_filename_from_filepath(data_file_location)] = \
-                create_asc_data_obj(data_file_location)
+    for data_file_location, data_file_object in data_file_objects_dict.items():
+        adjusted_output_location = "./Output Files/Topo Adjusted Files/adj_" + data_file_location[data_file_location.rfind("\\") + 1:]
+        write_asc_data_file(data_file_object.adjust_with_topo(), adjusted_output_location)
 
-        for data_file_location, data_file_object in data_file_objects_dict.items():
-            adjusted_output_location = "./Output Files/Topo Adjusted Files/adj_" + data_file_location[data_file_location.rfind("\\") + 1:]
-            write_asc_data_file(data_file_object.adjust_with_topo(), adjusted_output_location)
-
-        print("The adjusted output can be found at 'Output Files' in the project root folder.")
-    else:
-        print("WARNING ::: No asc files found!!!!!")
+    print("The adjusted output can be found at './Output Files/Topo Adjusted Files/' in the project root folder.")
     return data_file_objects_dict
 
 
--- main.py
+++ main.py
@@ -1,16 +1,33 @@
 from UtilityFunctions import *
 from colorama import Fore
+from pprint import pprint
 
 all_asc_files = []
-data_file_location = {}  # stores datafile names and its file location
-data_file_objects = {}  # list of AscData class objects of all input data files
+data_file_location_lookup = {}  # stores datafile names and its file location
+data_file_objects_lookup = {}  # lookup of AscData class objects of all input data files
 
 while True:
     print(Fore.RED + "Hit 'Enter' to quit." + Fore.RESET)
     path = input(Fore.YELLOW + "Input the folder locations if any ::: " + Fore.RESET)
     if path:
-        all_asc_files = get_list_of_asc_input_files(path)
-        print(all_asc_files)
+        all_asc_file_paths = get_list_of_asc_input_file_paths(path)
+        # Evaluate input asc data
+        all_data_file_paths = [file for file in all_asc_file_paths if "topo.asc" not in file]
+        if all_data_file_paths:
+            print(str(len(all_data_file_paths)) + " data files found!!!")
+
+            # Maintain lookup for filename and file path
+            data_file_location_lookup = {extract_filename_from_filepath(file_path): file_path for file_path in
+                                         all_data_file_paths}
+            pprint(data_file_location_lookup)
+
+            # Maintain lookup for filename and AscData objects
+            data_file_objects_lookup = {extract_filename_from_filepath(file_path): create_asc_data_obj(file_path)
+                                        for file_path in all_data_file_paths}
+
+            asc_data_objects = export_topo_adjusted_data(all_data_file_paths)
+        else:
+            print("WARNING ::: No asc data files found!!!!!")
     else:
         print("Failed!!!!!!!!!!")
         break
